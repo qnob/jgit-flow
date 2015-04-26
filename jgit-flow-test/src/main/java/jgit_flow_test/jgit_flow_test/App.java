508bf04af1628d5021191f9ee6330d5f9b408b33
kuno@gmx.ch <kuno@gmx.ch>
2015-04-26 18:23:55 +0200

    setup maven project

---
+++ jgit-flow-test/src/main/java/jgit_flow_test/jgit_flow_test/App.java
@@ -0,0 +1,13 @@
+package jgit_flow_test.jgit_flow_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
